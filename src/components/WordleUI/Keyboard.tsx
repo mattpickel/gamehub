84638bb3f12284c7fe1868b7bd5a9984e661554b
mattpickel <mpickelprogramming@gmail.com>
2024-02-05 01:46:10 -0600

    prevent delete key from being styled like guessed letter

--- src/components/WordleUI/Keyboard.tsx
+++ src/components/WordleUI/Keyboard.tsx
@@ -21,7 +21,10 @@ const Keyboard: React.FC<KeyboardProps> = ({ onKeyPress, letterStatusList }) => 
             {rows.map((row, rowIndex) => (
                 <div key={rowIndex} className='flex justify-center space-x-1 mb-2'>
                     {row.map((key) => {
-                        const status = letterStatusList[key.charCodeAt(0) - 65]?.status;
+                        let status;
+                        if (key.length === 1) {
+                            status = letterStatusList[key.charCodeAt(0) - 65]?.status;
+                        }
                         let className = 'py-2 px-4 bg-gray-200 text-gray-800 font-semibold rounded shadow';
                         if (key.length > 1) {
                             className += ' flex-grow';
